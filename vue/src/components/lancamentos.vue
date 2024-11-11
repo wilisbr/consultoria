488cb5c476d388788a520ce1ac5c642231022905
Charles Wilis <charles.wilis@gmail.com>
2024-11-10 23:56:55 -0300

    menu de lancamentos ok

--- vue/src/components/lancamentos.vue
+++ vue/src/components/lancamentos.vue
@@ -1,9 +1,23 @@
 <template>
   
   <div class="container" >
-    
+    <select v-model="idDREMensal">
+      <option disabled value="">Selecione a data base</option>
+      <option v-for="DRE in DREs" :key="DRE.id" :value="DRE.id">
+        {{ DRE.ano }}-{{ DRE.mes }}
+      </option>
+    </select>
     <p>DREs</p>
     {{DREs}}
+    <br>
+    <p> Lancamentos: </p>
+    {{lancamentos}}
+    <br> <p>Tipo Lancamentos:</p>
+    {{tipo_lancamento_dre}}
+
+    <hr>
+    
+
   </div>
 </template>
 
@@ -13,22 +27,51 @@
         name: 'lancamentosCrud',
         data() {
             return {
-            DREs: null
+            DREs: null,
+            lancamentos: null,
+            idDREMensal: null,
+            tipo_lancamento_dre: null,
+            
+            
             }
         },
         created() {
             this.fetch_DREs(this.$route.params.id);
+            this.fetch_tipo_lancamento();
         },
         watch: {
             '$route.params.id'(newId) {
-            this.fetch_DREs(newId);
-        }
-  },
+                this.fetch_DREs(newId);
+            },
+            'idDREMensal'(newDREMensal) {
+                this.fetch_lancamentos(newDREMensal);
+            }
+        },
         methods: {
             async fetch_DREs(id) {
             await axios.get(`http://localhost:8000/api/dre_mensal/?empresa__id=${id}`)
                 .then(response => {
                 this.DREs = response.data;
+                console.log (this.DREs)
+                })
+                .catch(error => {
+                console.error('Error fetching empresa:', error);
+                });
+            },
+            async fetch_lancamentos(id_DREMensal) {
+            await axios.get(`http://localhost:8000/api/lancamento_dre/?DRE_mensal=${id_DREMensal}`)
+                .then(response => {
+                this.lancamentos = response.data;
+                console.log (this.DREs)
+                })
+                .catch(error => {
+                console.error('Error fetching empresa:', error);
+                });
+            },
+            async fetch_tipo_lancamento(){
+                await axios.get(`http://localhost:8000/api/tipo_lancamento_dre`)
+                .then(response => {
+                this.tipo_lancamento_dre = response.data;
                 console.log (this.DREs)
                 })
                 .catch(error => {
--- vue/src/main.js
+++ vue/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import VueCookies from 'vue-cookies';
 import router from './router'; // Import the router
 
+
 const app=createApp(App)
 
 app.use(VueCookies);
