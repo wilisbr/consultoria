52cf9c3dcdf36b0dd2f03ea7b20adb76fa36e57d
Charles Wilis <charles.wilis@gmail.com>
2024-11-28 19:37:08 -0300

    liberando_CORS_para_tudo

--- consultoria/settings.py
+++ consultoria/settings.py
@@ -152,7 +152,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",  # Add your frontend URL here
     "http://127.0.0.1:8000",
     "http://docker1.vps.webdock.cloud",
-    "*"
 ]
 
 # To allow specific headers and methods
