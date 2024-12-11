94dde57833b4a2902e3f504eb41fb8c57a367b62
Charles Wilis <charles.wilis@gmail.com>
2024-12-11 20:24:40 -0300

    alterando as portas

--- vue/src/main.js
+++ vue/src/main.js
@@ -5,7 +5,7 @@ import router from './router'; // Import the router
 
 
 const app=createApp(App)
-app.provide('apiUrl', `http://${window.location.hostname}:8000/api`);
+app.provide('apiUrl', `http://${window.location.hostname}:8001/api`);
 app.use(VueCookies);
 app.use(router);
 app.mount('#app')
